b09f91a4bffb0a819a91ecfb44730124b13d6ebe
chojuninengu <chojuninengu@gmail.com>
2025-03-10 11:55:09 +0100

    updates

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,5 +1,11 @@
 // TODO: Add some function with the name `call_me` without arguments or a return value.
 
+use core::num;
+
 fn main() {
-    call_me(); // Don't change this line
+    fn call_me(num1: i32) -> i32 {
+        let num1 = 23;
+        println!("the number is {num1}");
+        num1
+    } // Don't change this line
 }
